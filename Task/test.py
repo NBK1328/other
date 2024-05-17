2662734d84c2a26b817e6c3bbb5f9044a5ce65b0
NBK1328 <don.zadorn@yandex.ru>
2024-05-18 00:17:19 +0300

    newTsk

--- Task/test.py
+++ Task/test.py
@@ -1,7 +1,17 @@
-objects = [1,2,3,1,2,3,1]
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
 
-strok = list(map(str, objects))
+browser = webdriver.Chrome()
 
-print(*strok, id(strok), id(objects))
+browser.get("http://suninjuly.github.io/wait2.html")
 
+# говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+button = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.ID, "verify"))
+    )
+button.click()
+message = browser.find_element(By.ID, "verify_message")
 
+assert "successful" in message.text
